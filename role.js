2103e4717fc7f9503c7536e91801c2e347589c26
Nemo <arlequ1n@protonmail.com>
2020-03-22 05:06:01 +0100

    ajout de la gestion des permissions manquantes dans l'ajour de roles

--- role.js
+++ role.js
@@ -3,9 +3,11 @@ const operator = "!role ";
 
 function execute(message) {
     command = message.content.slice(operator.length,message.content.length);
-    args = command.split(" "); // array made of the options and the arguments of the command
-    option = args[0]; // we get the option of the command in a variable
-    reply = "";
+    var args = command.split(" "); // array made of the options and the arguments of the command
+    var option = args[0]; // we get the option of the command in a variable
+    var reply = "";
+    var user = message.member;
+    var errorList = ""; // This is the list of the roles that return an error
 
     if (option != "ajouter" && option != "supprimer"){
         message.channel.send("veuillez choisir l'option \"ajouter\" ou \"supprimer\" puis la liste des rôles à modifier");return;
@@ -17,37 +19,41 @@ function execute(message) {
     let roles = []; // empty array ton contain the roles to add or delete on the user
     
     // let's start by checking the arguments are correct (roles exist)
-    var errorMessage = "Les rôles que vous avez choisi n'existent pas sur ce serveur (";
     var roleErrors = 0;
 	args.forEach(function(item, key){
         tempRole = message.guild.roles.cache.find(role => role.name == item);
         if(tempRole == null){
-            errorMessage += " " + item + " ";
+            reply += "Le rôle **" + item + "** que vous avez choisi n'existe pas sur ce serveur";
             roleErrors ++;
         }else{
-            roles.push(tempRole);
+            roles.push(tempRole); // on ajoute le role temporaire dans la liste des rôles à traiter
         }
     });
-    errorMessage += ")\n";
-    user = message.member;
-    errorList = ""; // This is the list of the role already attributed when you want to add or not included when you want to delete
+
+    // TODO : change this in different functions ?
 	switch (option) {
 		case 'ajouter' :
             roles.forEach(function (item, key){
                 member = item.members.find(guildMember => guildMember.id == user.id);
+                // member is undefined when the user doesn't have the role yet
 
-                // we check that the user doesn't have yet the role or that the role is not empty
-                if ( member != undefined || item.members.size != 0){ 
-                   errorList += " " + item.name + " " ;
+                // we check that the user doesn't have yet the role
+                if ( member != undefined){ 
+                    errorList += item.name + " " ;
                 }else{
-                    message.member.roles.add(item);
-                    reply += "Vous avez été ajouté.e au rôle " + item.name + " !\n";
+                    addRole = message.member.roles.add(item) //we add the user to the role members
+                    .then(() => {
+                        reply += "Vous avez été ajouté.e au rôle **" + item.name + "** !\n";
+                    })
+                    .catch(() => { // this is in case the bot doesn't have permission to add the role
+                        //reply += "Impossible de vous ajouter au role **" + item.name + "**\n";
+                        console.error(user.user.username + " tried to add role " + item.name);
+                    });
                 }
             });
             if (errorList != ""){
-                errorMessage += "Le rôle " + errorList + " vous est déjà attribué.\n";
+                reply += "Les rôles suivants vous ont déjà été attribués : **" + errorList + "**\n";
             }
-            reply += errorMessage;
             message.channel.send(reply);
 			break;
 		case 'supprimer' :
