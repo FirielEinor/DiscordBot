cbf49516d5645fad9748d7fb0134c63c1e76ed03
Nemo <arlequ1n@protonmail.com>
2020-03-25 12:24:58 +0100

    gestion du cas où les noms de rôles comportent des caractères non alphanumériques et petits ajustements (suppression de fichiers inutiles)

--- role/role.js
+++ role/role.js
@@ -18,18 +18,21 @@ function getRoles(message, client) {
     if (tableRoles[0].toLowerCase() == "!" + command) {
         //message.channel.send("Veuillez indiquer au moins un rôle à modifier");
         listRole(message, client);
-        return [] ;
+        return [];
     }
 
     var tempRole = null; // to contain the potential roles on the server
     var errorRole = 0;
     var reply = "";
+    String.prototype.cleanup = function () {
+        return this.replace(/[^a-zA-Z0-9 +]+/g, "").trim();
+    }
 
     tableRoles.forEach(function (item, key) {
-        if(item == ""){
+        if (item == "") {
             return;
         }
-        tempRole = message.guild.roles.cache.find(role => role.name == item);
+        tempRole = message.guild.roles.cache.find(role => role.name.cleanup() == item);
         if (tempRole == null) { // first we check the role exists on the server
             reply += "Le rôle **" + item + "** n'existe pas sur ce serveur, faites une demande aux admins !\n";
             errorRole++;
@@ -53,19 +56,19 @@ function listRole(message, client) {
     var listDisplayRoles = "";
 
     // We find the highest position of the bot in hierarchy
-    allRoles.forEach((item, key)=> {
-        if (item.members.find(member => member.id == userID) != null && item.rawPosition > botMaxPosition){
+    allRoles.forEach((item, key) => {
+        if (item.members.find(member => member.id == userID) != null && item.rawPosition > botMaxPosition) {
             botMaxPosition = item.rawPosition;
         }
     });
 
     // We display all roles that are below the bot in hierarchy
-    allRoles.forEach((item,key) => {
-        if(item.managed == true){
+    allRoles.forEach((item, key) => {
+        if (item.managed == true) {
             delete allRoles[key];
             return;
         }
-        if(item.rawPosition < botMaxPosition && item.rawPosition != 0){
+        if (item.rawPosition < botMaxPosition && item.rawPosition != 0) {
             listDisplayRoles += "\n    - **" + item.name + "**";
         }
     });
