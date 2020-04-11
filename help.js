406dd3d0fa6e21956f3fbdd3e032ddf69e66fac3
Nemo <arlequ1n@protonmail.com>
2020-04-11 15:41:37 +0200

    bug fix de la fonction help : quand on précise une commande la fonction affiche correctepent l'aide

--- help.js
+++ help.js
@@ -2,20 +2,22 @@ module.exports = execute;
 const fs = require('fs');
 const utils = require('./command.js');
 
-function execute(message){
-    if (fs.existsSync('./help.json')){
+function execute(message) {
+    if (fs.existsSync('./help.json')) {
         var jsonString = fs.readFileSync('./help.json');
         var manual = JSON.parse(jsonString);
-    }else{
+    } else {
         console.log("Error : Help command required when no help.json is accessible.");
         return;
     }
 
+
     args = utils.getArgs(message); // array made of the options and the arguments of the command
 
-    if (manual[args] == undefined || args[0] == ''){
+    if (manual[args] == undefined || args[0] == '') {
         message.channel.send(manual['default'].text);
         return;
+
     }
 
     message.channel.send(manual[args[0]].text);
